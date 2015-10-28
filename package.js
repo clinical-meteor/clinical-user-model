bd8ff8bd8eb2c7eeed5fcddd459db7caaab08c87
Abigail Watson <awatson1978@gmail.com>
2015-10-27 17:06:37 -0700

    normalizing package versions

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "clinical:user-model",
   summary: "A social user package",
-  version: "1.2.1",
+  version: "1.2.2",
   git: "https://github.com/clinical-meteor/clinical-user-model.git"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom("1.0.2.1");
+  api.versionsFrom('1.1.0.2');
 
   api.use([
         "socialize:base-model@0.2.3",
