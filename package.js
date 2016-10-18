8e35669102ab555c36e79ceb2ea37a0905472f03
Abigail Watson <awatson1978@gmail.com>
2016-10-18 05:38:33 -0500

    UserIdExtension

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "clinical:user-model",
   summary: "A social user package",
-  version: "1.4.9",
+  version: "1.5.0",
   git: "https://github.com/clinical-meteor/user-model.git"
 });
 
@@ -12,6 +12,7 @@ Package.onUse(function (api) {
   api.use('templating');
   api.use('accounts-base');
   api.use("clinical:base-model@1.3.5");
+  api.use('aldeed:simple-schema@1.3.3');
 
   api.imply([
     "clinical:base-model",
@@ -22,6 +23,7 @@ Package.onUse(function (api) {
   api.addFiles("lib/user-model.js");
 
   api.export("User");
+  api.export("UserIdExtension");
 });
 
 
