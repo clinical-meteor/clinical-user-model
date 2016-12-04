6ebbb5bd490347e19f80bbd01b7b109944f04c41
Abigail Watson <awatson1978@gmail.com>
2016-12-03 21:11:04 -0600

    circle.yml

--- package.js
+++ package.js
@@ -1,43 +1,24 @@
 Package.describe({
   name: "clinical:user-model",
   summary: "A social user package",
-  version: "1.5.1",
+  version: "1.6.0",
   git: "https://github.com/clinical-meteor/user-model.git"
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
-  api.use('meteor-platform');
-  api.use('templating');
-  api.use('accounts-base');
+  api.use('accounts-base@1.2.14');
   api.use("clinical:base-model@1.3.5");
   api.use('aldeed:simple-schema@1.3.3');
 
   api.imply([
-    "clinical:base-model",
+    "clinical:base-model@1.3.5",
     "accounts-base"
   ]);
 
-  api.addFiles("client/template.helpers.js", 'client');
   api.addFiles("lib/user-model.js");
 
   api.export("User");
   api.export("UserIdExtension");
-});
-
-
-
-
-Package.onTest(function (api) {
-  api.versionsFrom('1.1.0.3');
-  api.use('meteor-platform');
-  api.use('accounts-base@1.2.0');
-  api.use('accounts-password@1.1.1');
-  api.use('autopublish@1.0.3');
-  api.use('insecure@1.0.3');
-  api.use('tinytest@1.0.5');
-  api.use('clinical:verification');
-  api.use('clinical:user-model');
-
 });
