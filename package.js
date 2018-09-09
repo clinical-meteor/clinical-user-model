cadaa6b27c97b19e44dd2f516e11ab8188031e69
Abigail Watson <abigailwatson@Abigails-Mac-mini-379644.local>
2018-09-08 22:52:38 -0500

    simpl-schema

--- package.js
+++ package.js
@@ -1,24 +1,28 @@
 Package.describe({
   name: "clinical:user-model",
   summary: "A social user package",
-  version: "1.6.0",
+  version: "1.6.1",
   git: "https://github.com/clinical-meteor/user-model.git"
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
-  api.use('accounts-base@1.2.14');
-  api.use("clinical:base-model@1.3.5");
-  api.use('aldeed:simple-schema@1.3.3');
+  api.use('accounts-base@1.4.2');
+  api.use("clinical:base-model@1.4.0");
 
   api.imply([
-    "clinical:base-model@1.3.5",
-    "accounts-base@1.2.14"
+    "clinical:base-model@1.4.0",
+    "accounts-base@1.4.2"
   ]);
 
   api.addFiles("lib/user-model.js");
 
   api.export("User");
   api.export("UserIdExtension");
+});
+
+
+Npm.depends({
+  "simpl-schema": "1.5.3"
 });
