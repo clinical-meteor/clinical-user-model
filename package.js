d84e610d7107016ce4510ec9baac5f088dabcaa8
Abigail Watson <awatson1978@gmail.com>
2019-10-31 15:10:20 -0500

    removed old templating; Bert alerting

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "clinical:user-model",
   summary: "A social user package",
-  version: "1.6.5",
+  version: "1.7.0",
   git: "https://github.com/clinical-meteor/user-model.git"
 });
 
@@ -9,11 +9,11 @@ Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
   api.use('accounts-base@1.4.2');
-  api.use("clinical:base-model@1.4.0");
-  api.use('ecmascript@0.9.0');
+  api.use("clinical:base-model@1.5.0");
+  api.use('ecmascript@0.12.4');
 
   api.imply([
-    "clinical:base-model@1.4.0",
+    "clinical:base-model@1.5.0",
     "accounts-base@1.4.2"
   ]);
 
